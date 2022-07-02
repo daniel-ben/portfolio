1bcb66e448b8f9df59e221f3138ee1471e0129f3
Daniel Ben <contato.bendaniel@gmail.com>
2022-07-01 21:26:15 -0400

    add header effect on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import MyHeader from './layouts/header'
+import Header from './layouts/header'
 import Homepage from './pages/homepage';
 import Work from './pages/work'
 import About from './pages/about';
@@ -6,31 +6,16 @@ import Contact from './pages/contact/indext';
 
 import React from 'react';
 // @ts-ignore
-import {SectionsContainer, Section, Header} from 'react-fullpage';
 
 function App() {
 
-  let options = {
-    sectionClassName:     'section',
-    anchors:              ['sectionOne', 'sectionTwo', 'sectionThree', 'sectionFour'],
-    scrollBar:            false,
-    navigation:           true,
-    verticalAlign:        false,
-    sectionPaddingTop:    '60px',
-    arrowNavigation:      true
-  };
-
   return (
     <>
-      <Header>
-        <MyHeader />
-      </Header>
-      <SectionsContainer className="container" {...options}>
-        <Section><Homepage /></Section>
-        <Section><Work /></Section>
-        <Section><About /></Section>
-        <Section><Contact /></Section>
-      </SectionsContainer>
+      <Header />
+      <Homepage />
+      <Work />
+      <About />
+      <Contact />
     </>
   )
 }
--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,25 +1,50 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 function Header() {
-    return (
-        <header className='header'>
-        <span className='header__name'>Daniel Ben</span>
-        <ul className='header__nav'>
-          <li className='current-page header__nav-item'>
-            <a href="#home">Home</a>
-          </li>
-          <li className='header__nav-item'>
-            <a href="#work">Work</a>
-          </li>
-          <li className='header__nav-item'>
-            <a href="#about">About</a>
-          </li>
-          <li className='header__nav-item'>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </header> 
-    )
+  const [activePage, setActivePage] = useState('home-nav');
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => handleHeaderActivePage(activePage, setActivePage))
+  })
+
+  return (
+    <header className="header">
+      <span className="header__name">Daniel Ben</span>
+      <ul className="header__nav">
+        <li className="current-page header__nav-item" id='home-nav'>
+          <a href="#home">Home</a>
+        </li>
+        <li className="header__nav-item" id='work-nav'>
+          <a href="#work">Work</a>
+        </li>
+        <li className="header__nav-item" id='about-nav'>
+          <a href="#about">About</a>
+        </li>
+        <li className="header__nav-item" id='contact-nav'>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
+
+function handleHeaderActivePage(activePage: string, setActivePage: React.Dispatch<React.SetStateAction<string>>) {
+  const navItems = ['home-nav', 'work-nav', 'about-nav', 'contact-nav'];
+
+  const windowHeight = window.innerHeight;
+  const currentScrollHeight = window.scrollY;
+  const currentPage = navItems[Math.floor(currentScrollHeight / (windowHeight - 60))];
+
+  if (currentPage !== activePage) {
+    const previousPage = document.getElementById(activePage);
+    previousPage?.classList.remove('current-page');
+
+    const newPage = document.getElementById(currentPage);
+    newPage?.classList.add('current-page');
+
+    setActivePage(currentPage);
+  }
+}
